cb338b8e7d5b9266e59bea2d66c79ae3ad833c04
Carlos Castillo fernandez <ccastill@c2r7s3.42madrid.com>
2020-01-27 15:48:22 +0100

    1

--- get_next_line.c
+++ get_next_line.c
@@ -6,28 +6,15 @@
 /*   By: ccastill <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/12/05 23:17:01 by ccastill          #+#    #+#             */
-/*   Updated: 2020/01/27 14:00:58 by ccastill         ###   ########.fr       */
+/*   Updated: 2020/01/27 15:47:54 by ccastill         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-<<<<<<< HEAD
-static int ft_compare(bwr, fd, s, line)
-=======
-static int	ft_comp_new_line(char **s, char **line)
->>>>>>> 64473c0440876faa18ca973ef4503613798f07fe
-{
-
-
-
-
-	
-}
 
 int			get_next_line(int fd, char **line)
 {
-<<<<<<< HEAD
 	char		*buff; // Creamos una cadena char para almacenar el tamaño de memoria necesario.
 	static char *s[4096]; // Creamos una cadena estática que permita manejar el contenido del fd a lo alrgo de las funciones.
 	char		*tmp; // Cadena temporal donde almacenaremos 
@@ -38,17 +25,6 @@ int			get_next_line(int fd, char **line)
 		return (-1); // En caso de que no se pase un fd o un line menor que cero o cero, la función devolverá -1 indicando un error.
 		
 	while ((bwr = read(fd, buff, BUFFER_SIZE)) > 0) // Mientras que el bwr sea mayor que 0 entra. Lee el fd, lo almacena en el array (buff), leerá porciones del tamaño inicado por BUFFER_SIZE.
-=======
-	char		*buff;
-	static char *s[4096];
-	char		*tmp;
-	ssize_t		bwr;
-
-	if (!(buff = malloc(sizeof(char) * (BUFFER_SIZE + 1)))
-		|| fd < 0 || line == 0)
-		return (-1);
-	while ((bwr = read(fd, buff, BUFFER_SIZE)) > 0)
->>>>>>> 64473c0440876faa18ca973ef4503613798f07fe
 	{
 		buff[bwr] = '\0'; // Añadimos el NULL.
 		if (s[fd] == NULL) // Si el fd es NULL, 
@@ -62,14 +38,8 @@ int			get_next_line(int fd, char **line)
 		if (ft_strchr(s[fd], '\n')) // Si encuentra un salto de línea en s, break.
 			break ;
 	}
-<<<<<<< HEAD
+
 	free(buff); //Se libera el buff
 	buff = NULL; // Se iguala a NULL
 	return (ft_compare(bwr, fd, s, line));
-}
-=======
-	free(buff);
-	buff = NULL;
-	return (ft_comp(bwr, fd, s, line));
-}
->>>>>>> 64473c0440876faa18ca973ef4503613798f07fe
+
